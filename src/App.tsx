2be62468ad7bb79d910fd44bca58ae91f1c3e761
Balazs Radvanyi <balazs@riltech.co>
2026-09-25 11:45:47 +0200

    feat: implemente the React Keyboard feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
 import React from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+type State = {
+  message: string;
+};
+
+export class App extends React.Component<State> {
+  state: Readonly<State> = {
+    message: 'Nothing was pressed yet',
+  };
+
+  keyUpHandler = (event: KeyboardEvent) => {
+    this.setState({ message: `The last pressed key is [${event.key}]` });
+  };
+
+  componentDidMount() {
+    document.addEventListener('keyup', this.keyUpHandler);
+  }
+
+  componentWillUnmount(): void {
+    document.removeEventListener('keyup', this.keyUpHandler);
+  }
+
+  render() {
+    const { message } = this.state;
+
+    return (
+      <div className="App">
+        <p className="App__message">{message}</p>
+      </div>
+    );
+  }
+}
